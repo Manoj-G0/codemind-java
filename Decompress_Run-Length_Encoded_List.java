f4f67db8af7ce536d859dbfa6d6f36ed3650f821
Manoj <97424621+manojmanu34@users.noreply.github.com>
2022-10-29 18:32:47 +0530

    import java.util.*; class manoj{     public static void main(String[] args){         Scanner t=new Scanner(System.in);         int n=t.nextInt();         int a[]=new int[100];         for(int i=0;i<n;i++)         a[i]=t.nextInt();         for(int i=0;i<a[0];i++)         {             System.out.printf("%d ",a[1]);         }         for(int i=0;i<a[2];i++)         {             System.out.printf("%d ",a[3]);         }     } }

---
+++ Decompress_Run-Length_Encoded_List.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class manoj{
+    public static void main(String[] args){
+        Scanner t=new Scanner(System.in);
+        int n=t.nextInt();
+        int a[]=new int[100];
+        for(int i=0;i<n;i++)
+        a[i]=t.nextInt();
+        for(int i=0;i<a[0];i++)
+        {
+            System.out.printf("%d ",a[1]);
+        }
+        for(int i=0;i<a[2];i++)
+        {
+            System.out.printf("%d ",a[3]);
+        }
+    }
+}
