7a7c40b02c5c01abc48b15b9924e0fa846b9d48f
Manoj <97424621+manojmanu34@users.noreply.github.com>
2022-10-31 20:07:11 +0530

    import java.util.Scanner; class manoj{     public static boolean isvowel(String s,int i,int j){         int c=0;         for(int k=i;k<j;k++)         {             if(s.charAt(k)=='a'|| s.charAt(k)=='e'|| s.charAt(k)=='i'|| s.charAt(k)=='o'|| s.charAt(k)=='u')             {                 c++;             }         }         if(c==j-i)         return true;         return false;     }     public static void main(String[] args){         Scanner t=new Scanner(System.in);         String s=t.next();         s=s.toLowerCase();         System.out.println(s);
        }
    }

---
+++ To_Lower_Case.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class manoj{
+    public static boolean isvowel(String s,int i,int j){
+        int c=0;
+        for(int k=i;k<j;k++)
+        {
+            if(s.charAt(k)=='a'|| s.charAt(k)=='e'|| s.charAt(k)=='i'|| s.charAt(k)=='o'|| s.charAt(k)=='u')
+            {
+                c++;
+            }
+        }
+        if(c==j-i)
+        return true;
+        return false;
+    }
+    public static void main(String[] args){
+        Scanner t=new Scanner(System.in);
+        String s=t.next();
+        s=s.toLowerCase();
+        System.out.println(s);
+        
+    }
+}
