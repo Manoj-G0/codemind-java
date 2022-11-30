94a0ed0154e1dafd86c0c99b2df960f4fa896a86
Manoj <97424621+manojmanu34@users.noreply.github.com>
2022-11-30 19:51:01 +0530

    import java.util.Scanner; class manoj{     public static void main(String[] args){         Scanner t=new Scanner(System.in);         String s=t.next();         int a[]=new int[100];         for(int i=0;i<s.length();i++)         {             if(s.charAt(i)=='M')             a[i]=1000;             else if(s.charAt(i)=='D')             a[i]=500;             else if(s.charAt(i)=='C')             a[i]=100;             else if(s.charAt(i)=='L')             a[i]=50;             else if(s.charAt(i)=='X')             a[i]=10;             else if(s.charAt(i)=='V')             a[i]=5;             else if(s.charAt(i)=='I')             a[i]=1;         }         int sum=0;         for(int i=0;i<s.length();i++)         {             if(a[i]>=a[i+1] || i==s.length()-1)             {                 sum+=a[i];             }             else             {                 sum-=a[i];             }         }         System.out.println(sum);     } }

--- Roman_to_Integer.java
+++ Roman_to_Integer.java
@@ -1,45 +1,37 @@
 import java.util.Scanner;
 class manoj{
-    public static boolean isprime(int n)
-    {
-        if(n<=1)
-        return false;
-        for(int i=2;i<=n/2;i++)
-        {
-            if(n%i==0)
-            return false;
-        }
-        return true;
-    }
     public static void main(String[] args){
         Scanner t=new Scanner(System.in);
-        String k=t.nextLine();
-        char s[]=k.toCharArray();
+        String s=t.next();
         int a[]=new int[100];
-        for(int i=0;i<k.length();i++)
+        for(int i=0;i<s.length();i++)
         {
-            if(s[i]=='I')
-            a[i]=1;
-            else if(s[i]=='V')
-            a[i]=5;
-            else if(s[i]=='X')
-            a[i]=10;
-            else if(s[i]=='L')
-            a[i]=50;
-            else if(s[i]=='C')
-            a[i]=100;
-            else if(s[i]=='D')
-            a[i]=500;
-            else if(s[i]=='M')
+            if(s.charAt(i)=='M')
             a[i]=1000;
+            else if(s.charAt(i)=='D')
+            a[i]=500;
+            else if(s.charAt(i)=='C')
+            a[i]=100;
+            else if(s.charAt(i)=='L')
+            a[i]=50;
+            else if(s.charAt(i)=='X')
+            a[i]=10;
+            else if(s.charAt(i)=='V')
+            a[i]=5;
+            else if(s.charAt(i)=='I')
+            a[i]=1;
         }
         int sum=0;
-        for(int i=0;i<k.length();i++)
+        for(int i=0;i<s.length();i++)
         {
-            if(i==k.length()-1 || a[i]>=a[i+1])
-            sum+=a[i];
+            if(a[i]>=a[i+1] || i==s.length()-1)
+            {
+                sum+=a[i];
+            }
             else
-            sum-=a[i];
+            {
+                sum-=a[i];
+            }
         }
         System.out.println(sum);
     }
